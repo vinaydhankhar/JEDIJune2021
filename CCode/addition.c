d09bd00e65d3e9a225ecab3bf0756ba1523f7ff2
Vinay Dhankhar <vinaydhankhar6@gmail.com>
2021-06-17 10:24:25 +0530

    Changes committed: 	CCode/addition.c: function to add 4 numbers added 	JavaCode/HelloWorld.java: method for helloworld msg created

--- CCode/addition.c
+++ CCode/addition.c
@@ -7,3 +7,7 @@ int sum(int x,int y){
 int sumthree(int x,int y,int z){
 	return x+y+z;
 }
+
+int sumfour(int x,int y,int z,int t){
+	return x+y+z+t;
+}
--- JavaCode/HelloWorld.java
+++ JavaCode/HelloWorld.java
@@ -2,6 +2,9 @@ package learning_java;
 
 class HelloWorld{
 	public static void main(String args[]){
+		helloWorld();
+	}
+	public static void helloWorld(){
 		System.out.println("hello world");
 	}
 }
